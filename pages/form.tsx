1fd36f4ce7117ef2d257c314eb21fd663220f95d
Hendrik Nieländer <hendrik@nielaender.de>
2023-04-23 23:38:52 +0200

    feat: upgrade rjsf v5

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Form from "@rjsf/chakra-ui";
+import validator from "@rjsf/validator-ajv8";
 import step1schema from "../schema/page1.json";
 import step2schema from "../schema/page2.json";
 
@@ -7,7 +8,7 @@ export default function HookForm() {
   const [formData, setFormData] = useState();
   const [step, setStep] = useState(1);
 
-  const onSubmit = ({formData}) => {
+  const onSubmit = ({formData}: any) => {
   	if (step === 1) {
       setFormData(formData)
       console.log(formData);
@@ -16,11 +17,12 @@ export default function HookForm() {
       alert("You submitted " + JSON.stringify(formData, null, 2));
     }
   }
-  
+
   return (
     <Form schema={step === 1 ? step1schema : step2schema}
           onSubmit={onSubmit}
           formData={formData}
-    />  
+          validator={validator}
+    />
   )
 }
